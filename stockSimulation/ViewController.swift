29fc9ce32cde73c2baf88c4fb87dcefd31bd4dc7
Ariel Cobena <cobenaa11@gmail.com>
2021-04-13 22:16:56 -0400

    Initial Commit

---
+++ stockSimulation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stockSimulation
+//
+//  Created by Ariel Cobena on 4/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
