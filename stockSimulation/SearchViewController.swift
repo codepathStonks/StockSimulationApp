340709f39c706da03c7ee888f7b8ba2d0914a2dd
Ariel Cobena <cobenaa11@gmail.com>
2021-04-24 11:27:01 -0400

    Adds search functions

--- stockSimulation/SearchViewController.swift
+++ stockSimulation/SearchViewController.swift
@@ -8,11 +8,58 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    let API_KEY = "OVULR05CBMMN9THU"
 
+    var stockResults = [[String:Any]]()
+    
+    let symbolText = "TSLA"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        stockInformation(ticker: symbolText)
         // Do any additional setup after loading the view.
+    }
+    
+    func stockSearch(symbolText: String) {
+        
+        let url = URL(string: "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + symbolText + "&apikey=" + API_KEY)!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+             // This will run when the network request returns
+             if let error = error {
+                    print(error.localizedDescription)
+             } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                    
+                    print(dataDictionary)
+
+             }
+        }
+        task.resume()
+        
+    }
+    
+    func stockInformation(ticker: String) {
+        
+        let url = URL(string: "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + ticker + "&apikey=" + API_KEY)!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+             // This will run when the network request returns
+             if let error = error {
+                    print(error.localizedDescription)
+             } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                    
+                    print(dataDictionary)
+
+             }
+        }
+        task.resume()
+        
     }
     
 
