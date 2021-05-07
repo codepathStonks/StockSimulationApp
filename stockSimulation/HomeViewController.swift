87c9518bad6f20700755ee7d66b94921f59c1285
Calvin <cwang4749@bths.edu>
2021-05-06 21:14:17 -0400

    Moved over to storing data into arrays, added deselection of cells

--- stockSimulation/HomeViewController.swift
+++ stockSimulation/HomeViewController.swift
@@ -24,6 +24,10 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     var count = 0
     var totalValue = 0.0
     
+    var allStockPrices = [""]
+    var allStockChanges = [""]
+    let group = DispatchGroup()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         home_tableView.dataSource = self
@@ -43,7 +47,63 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         HomeBalanceLabel.text = "$"
         HomeBalanceLabel.text?.append(String(balance))
         
+        
+        
+        var symbolText = ""
+        var tickers = [""]
+        
+        let q2 = PFQuery(className: "Portfolio")
+        q2.whereKey("user", equalTo: curr_user!)
+        if let portfolio = try? q2.findObjects()
+        {
+            for a in 0...portfolio.count - 1 {
+                let obj = portfolio[a]
+                symbolText = obj["ticker"] as! String
+                tickers.append(symbolText)
+            }
+        }
+        else {
+            print("error getting portfolio")
+        }
+        
+        group.enter()
+        for b in 1...tickers.count - 1{
+            symbolText = tickers[b]
+            let url = URL(string: "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + symbolText + "&apikey=" + API_KEY)!
+            let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+            let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+            let task = session.dataTask(with: request) { (data, response, error) in
+                 // This will run when the network request returns
+                 if let error = error {
+                        print(error.localizedDescription)
+                 } else if let data = data {
+                        let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                        print(dataDictionary)
+                    self.stockResults = dataDictionary["Global Quote"] as! [String : Any]
+                    self.allStockPrices.append(self.stockResults["05. price"] as! String)
+                    self.allStockChanges.append(self.stockResults["09. change"] as! String)
+                    
+                    let change = self.stockResults["09. change"] as! String
+                    let c = Double(change)
+                    self.totalChange += c!
+                    //find individual value of each stock
+                    let price = self.stockResults["05. price"]! as! String
+                    let p = Double(price)
+                    self.totalValue += p!
+                 }
+            }
+            task.resume()
+        }
+        group.leave()
+        
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        if let selectionIndexPath = home_tableView.indexPathForSelectedRow{
+                self.home_tableView.deselectRow(at: selectionIndexPath, animated: animated)
+        }
     }
     
     @IBAction func onLogoutButton(_ sender: Any) {
@@ -74,7 +134,6 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         var symbolText = ""
-        var price = ""
         let cell = tableView.dequeueReusableCell(withIdentifier: "homeCell") as! homeCell
         
         //Placeholders
@@ -93,57 +152,74 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
             print("error getting portfolio")
         }
         
-        let url = URL(string: "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + symbolText + "&apikey=" + API_KEY)!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        let task = session.dataTask(with: request) { (data, response, error) in
-             // This will run when the network request returns
-             if let error = error {
-                    print(error.localizedDescription)
-             } else if let data = data {
-                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                    print(dataDictionary)
-                self.stockResults = dataDictionary["Global Quote"] as! [String : Any]
-                print(self.stockResults)
-                print("-----------------")
-                
-                //set price label = current price of stock
-                price = self.stockResults["05. price"]! as! String
-                let p = Double(price)
-                cell.PriceLabel.text = String(format: "$%.2f", p!)
-                
-                //find individual changes of each stock
-                let change = self.stockResults["09. change"] as! String
-                let c = Double(change)
-                if c! < 0 {
-                    cell.PriceLabel.textColor = UIColor.red
-                }
-                else {
-                    cell.PriceLabel.textColor = UIColor.green
-                }
-                
-                let group = DispatchGroup()
-                group.enter()
-                self.totalChange += c!
-                print(self.totalChange)
-                //find individual value of each stock
-                self.totalValue += p!
-                print(self.totalValue)
-                print(indexPath.row)
-                print(self.count)
-                group.leave()
-                
-                if indexPath.row == self.count - 1{
-                    self.changeChangeLabel()
-                    self.changeTotalLabel()
-                    print(self.totalChange)
-                    print(self.totalValue)
-                }
-                
-
-             }
+        group.notify(queue: .main) {
+            //get price
+            let p = Double(self.allStockPrices[indexPath.row + 1])
+            cell.PriceLabel.text = String(format: "$%.2f", p!)
+            let change = self.stockResults["09. change"] as! String
+            //get change
+            let c = Double(self.allStockChanges[indexPath.row + 1])
+            if c! < 0 {
+                cell.PriceLabel.textColor = UIColor.red
+            }
+            else {
+                cell.PriceLabel.textColor = UIColor.green
+            }
+            self.changeTotalLabel()
+            self.changeChangeLabel()
         }
-        task.resume()
+        
+//        let url = URL(string: "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + symbolText + "&apikey=" + API_KEY)!
+//        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+//        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+//        let task = session.dataTask(with: request) { (data, response, error) in
+//             // This will run when the network request returns
+//             if let error = error {
+//                    print(error.localizedDescription)
+//             } else if let data = data {
+//                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+//                    print(dataDictionary)
+//                self.stockResults = dataDictionary["Global Quote"] as! [String : Any]
+//                print(self.stockResults)
+//                print("-----------------")
+//
+//                //set price label = current price of stock
+//                let price = self.stockResults["05. price"]! as! String
+//                let p = Double(price)
+//                cell.PriceLabel.text = String(format: "$%.2f", p!)
+//
+//                //find individual changes of each stock
+//                let change = self.stockResults["09. change"] as! String
+//                let c = Double(change)
+//                if c! < 0 {
+//                    cell.PriceLabel.textColor = UIColor.red
+//                }
+//                else {
+//                    cell.PriceLabel.textColor = UIColor.green
+//                }
+//
+//                let group = DispatchGroup()
+//                group.enter()
+//                self.totalChange += c!
+//                print(self.totalChange)
+//                //find individual value of each stock
+//                self.totalValue += p!
+//                print(self.totalValue)
+//                print(indexPath.row)
+//                print(self.count)
+//                group.leave()
+//
+//                if indexPath.row == self.count - 1{
+//                    self.changeChangeLabel()
+//                    self.changeTotalLabel()
+//                    print(self.totalChange)
+//                    print(self.totalValue)
+//                }
+//
+//
+//             }
+//        }
+//        task.resume()
         
         return cell
     }
